8a007707d6d5210aa78acb50397ccb86ee30f0bb
zain <paryanizain@gmail.com>
2024-11-25 04:57:40 +0500

    first commit

---
+++ class-10-hm/my-app/app/components/State.tsx
@@ -0,0 +1,28 @@
+"use client";
+import { useState } from "react";
+function State() {
+  let [counter, setCounter] = useState(0);
+  return (
+    <div className="bg-slate-900 flex place-content-center py-8">
+      <div className="w-48 md:w-56 lg:w-96 flex bg-slate-900 flex-col items-center rounded-md gap-10 border-2 p-20">
+        <p className="font-samibold">{counter}</p>
+        <div className="text-center">
+          <button
+            onClick={() => setCounter(counter + 1)}
+            className="border border-black border-x-2 bg-white size-12 rounded-full"
+          ></button>
+          <div>Count</div>
+        </div>
+        <div className="text-center">
+          <button
+            onClick={() => setCounter(0)}
+            className="border border-black border-x-2 size-6 bg-white rounded-full"
+          ></button>
+          <div className="text-xs">Reset</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default State;
---
+++ class-10-hm/my-app/app/page.tsx
@@ -0,0 +1,8 @@
+import State from "./components/State";
+export default function Home() {
+  return (
+  <div>
+    <State />
+  </div>
+  );
+}
