e11b832259ade7cdff58f0848be93df87db154f4
zain <paryanizain@gmail.com>
2024-11-25 05:03:22 +0500

    1st  change

--- class-10-hm/my-app/app/components/State.tsx
+++ class-10-hm/my-app/app/components/State.tsx
@@ -1,21 +1,21 @@
 "use client";
 import { useState } from "react";
 function State() {
-  let [counter, setCounter] = useState(0);
+  let [counting, setCounting] = useState(0);
   return (
     <div className="bg-slate-900 flex place-content-center py-8">
       <div className="w-48 md:w-56 lg:w-96 flex bg-slate-900 flex-col items-center rounded-md gap-10 border-2 p-20">
-        <p className="font-samibold">{counter}</p>
+        <p className="font-samibold">{counting}</p>
         <div className="text-center">
           <button
-            onClick={() => setCounter(counter + 1)}
+            onClick={() => setCounting(counting + 1)}
             className="border border-black border-x-2 bg-white size-12 rounded-full"
           ></button>
           <div>Count</div>
         </div>
         <div className="text-center">
           <button
-            onClick={() => setCounter(0)}
+            onClick={() => setCounting(0)}
             className="border border-black border-x-2 size-6 bg-white rounded-full"
           ></button>
           <div className="text-xs">Reset</div>
